e2c3d1f9ffd992c5d4cac4e8e0f21153d6f5a141
kwonkeonhyeong <rjsgud19566@gmail.com>
2024-04-02 17:58:32 +0900

    백준 5597번: 권건형

--- algorithm/src/exam_0330/Main.java
+++ algorithm/src/exam_0330/Main.java
@@ -1,7 +1,34 @@
 package exam_0330;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int[] input = new int[28];
+        int[] answer = new int[2];
+
+        for (int i = 0; i < 28; i++) {
+            input[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(input);
+
+        int inputIndex = 0;
+        int answerIndex = 0;
+
+        for (int i = 0; i < 30; i++) {
+            if(input[inputIndex] == i+1) {
+                inputIndex++;
+                continue;
+            }
+            answer[answerIndex++] = i+1;
+        }
+
+        for (int i : answer) {
+            System.out.println(i);
+        }
     }
 }
